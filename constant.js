155825bcea64e10b1cc1e225d904427a74d37053
manas-droid <manas.kalangan@gmail.com>
2021-08-09 17:23:31 +0530

    init

---
+++ constant.js
@@ -0,0 +1,16 @@
+
+export function addTextToSpan(text) {
+  const that = getConstant();
+  text.innerHTML=that.Cashless_Garages;
+}
+
+
+
+
+
+function getConstant(){
+  return {
+    "Cashless_Garages"   : "7100+",
+    "someother_constant" : "4903",
+  }
+}
